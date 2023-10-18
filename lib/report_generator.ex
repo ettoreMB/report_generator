2973a428deacde775406f863f82f4adf35b15e7a
ettore <ettore.barbosa@gmail.com>
2023-10-18 11:22:42 -0300

    Parseando nosso arquivo

--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -2,7 +2,10 @@ defmodule ReportGenerator do
   def build(filename) do
     "reports/#{filename}"
     |> File.stream!()
-    |> Enum.map(fn ln -> parse_line(ln) end)
+    |> Enum.reduce(%{}, fn ln, report ->
+      [id, _food_name, price] = parse_line(ln)
+      Map.put(report, id, price)
+    end)
   end
 
   defp parse_line(line) do
@@ -10,6 +13,7 @@ defmodule ReportGenerator do
     |> String.trim()
     |> String.split(",")
     |> List.update_at(2, &String.to_integer/1)
+
     # |> List.update_at(2, fn el -> String.to_integer(el)end)
   end
 end
