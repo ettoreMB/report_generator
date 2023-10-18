c42e431779b1c22aa832809abd186ea2211b4513
ettore <ettore.barbosa@gmail.com>
2023-10-18 10:29:53 -0300

    lendo um arquivo com stream

--- lib/report_generator.ex
+++ lib/report_generator.ex
@@ -1,10 +1,15 @@
 defmodule ReportGenerator do
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handleFile()
+    |> File.stream!()
+    |> Enum.map(fn ln -> parse_line(ln) end)
   end
 
-  defp handleFile({:ok, file_content}), do: file_content
-  defp handleFile({:error, reason}), do: reason
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+    # |> List.update_at(2, fn el -> String.to_integer(el)end)
+  end
 end
